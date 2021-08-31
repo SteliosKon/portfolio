0cbff60f458c404815a94b10bebc3f74e963f138
stelioskon <steliosnteloss@gmail.com>
2021-09-01 00:17:20 +0300

    init

--- commons/ui/CustomLink.jsx
+++ commons/ui/CustomLink.jsx
@@ -1,9 +1,16 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 
 const CustomLink = ({ href, children, ...props }) => {
+  const router = useRouter()
+  const style = {
+    color: router.asPath === href ? '#EEBF63' : '#2F2F2F',
+  }
   return (
     <Link href={href}>
-      <a {...props}>{children}</a>
+      <a {...props} style={style}>
+        {children}
+      </a>
     </Link>
   )
 }
--- components/content/Homepage/Intro.jsx
+++ components/content/Homepage/Intro.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef } from 'react'
 import { gsap } from 'gsap'
+import { InstagramIcon, FacebookIcon, LinkedInIcon } from 'commons/helpers/svgStore'
 const welcomeText = 'welcome to my website'
 
 const Intro = () => {
@@ -12,9 +13,43 @@ const Intro = () => {
 
   return (
     <div className='introduction'>
-      <h1>amasndasdasd</h1> <h1>amasndasdasd</h1> <h1>amasndasdasd</h1> <h1>amasndasdasd</h1> <h1>amasndasdasd</h1>{' '}
-      <h1>amasndasdasd</h1> <h1>amasndasdasd</h1> <h1>amasndasdasd</h1> <h1>amasndasdasd</h1> <h1>amasndasdasd</h1>{' '}
-      <h1>amasndasdasd</h1> <h1>amasndasdasd</h1> <h1>amasndasdasd</h1> <h1>amasndasdasd</h1>
+      <div className='info-container'>
+        <h4 className='hey'>Hey!</h4>
+        <h1 className='name'>
+          <span>
+            I&apos;m <span style={{ color: '#EEBF63' }}>Stelios Konstantelos</span>
+          </span>
+          <span>Front-end Dev.</span>
+        </h1>
+        <div className='text-base desc'>
+          <span>Minimalist with passion for coding.</span>
+          <span>Designing, implementing and testing UI components</span>
+        </div>
+        <div className='text-base social'>
+          <span>Find me</span>
+
+          <span>
+            <InstagramIcon />
+          </span>
+
+          <span>
+            <LinkedInIcon />
+          </span>
+
+          <span>
+            <FacebookIcon />
+          </span>
+        </div>
+        <div className='intro-btn'>
+          <button className='btn-filled'>
+            <span>Mail me</span>
+          </button>
+          <button className='btn-outlined'>
+            <span>Download CV</span>
+          </button>
+        </div>
+      </div>
+      <div className='image-container'>Column 2</div>
     </div>
   )
 }
